8bb95a3362af52d35ce789391219008f7e4a3854
Alberto Sola <albsolac@gmail.com>
2015-08-05 10:18:33 +0200

    Create numemory.js

---
+++ numemory.js
@@ -0,0 +1,30 @@
+/*
+    Numemory
+    =================================
+    Memory game in <140 characters.
+
+    License
+    =================================
+    MIT License: https://github.com/pirobtumen/numemory/blob/master/LICENSE.txt
+    
+    Author
+    =================================
+    @Pirobtumen, Alberto Sola
+
+    How to play ( example ):
+    =================================
+    
+    alert: 1
+    prompt: 1
+    
+    alert: 8
+    prompt: 18
+    
+    alert: 5
+    prompt: 185
+    
+    (...)
+
+*/
+
+function g(){c++,m=Math.floor(9*Math.random()),alert(m),n+=m,i=prompt(),i!=n?alert("Result: "+c):g()}var c=0,p="a",n="",m,i;g();
